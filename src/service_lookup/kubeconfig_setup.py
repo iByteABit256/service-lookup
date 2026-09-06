e0cb7760ca7bbe4729fa6cec21d3925e430e90d1
Pavlos Smith <paulsmith4561@gmail.com>
2026-09-06 13:29:36 +0300

    Fixed cache path for Windows & Mac using user_cache_dir

--- src/service_lookup/kubeconfig_setup.py
+++ src/service_lookup/kubeconfig_setup.py
@@ -5,6 +5,7 @@ import os
 import time
 from pathlib import Path
 
+from platformdirs import user_cache_dir
 from ruamel.yaml import YAML
 
 from .kubectl_service import KubectlService
@@ -35,7 +36,7 @@ def get_lens_kubeconfig_for_namespace(namespace, request_timeout, cache_invalida
         print("No kubeconfig files found in the Lens directory.")
         return None
 
-    cache_file = Path.home() / ".cache" / "service-lookup" / "namespaces.yaml"
+    cache_file = Path(user_cache_dir("service-lookup", appauthor=False)) / "namespaces.yaml"
     cache_updated = False
     if not cache_file.exists():
         cache_file.parent.mkdir(exist_ok=True, parents=True)
